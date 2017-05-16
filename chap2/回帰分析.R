dcdd599a5b72d270a27f2510ad1ff59ad9d0224e
futurebridge <kengo.nagahashi@futurebridge.jp>
2017-05-16 18:14:13 +0900

    change
    change

--- chap2/回帰分析.R
+++ chap2/回帰分析.R
@@ -1,6 +1,9 @@
-children = read.csv("https://raw.githubusercontent.com/futurebridge/RBooks/master/children.csv", header=TRUE)
-
+children = read.csv("https://raw.githubusercontent.com/futurebridge/RBooks/master/chap2/children.csv", header=TRUE)
 summary(children)
+
+#NAについて削除
+children=na.omit(children)
+
 
 #年齢と体重との相関係数を算出
 cor.test(children$age,children$weight)
@@ -10,43 +13,40 @@ cor.test(children$age,children$length)
 ＃２ｘ２のグラフを出力　最後に”;”をつける
 par(mfrow=c(2,2));
 ＃年齢と身長のグラフを描画
-plot(children$age, children$length)
+plot(children$age, children$length,main="年齢と身長",ylab="年齢",xlab="身長")
 ＃年齢と体重のグラフを描画
-plot(children$age, children$weight)
+plot(children$age, children$weight,main="年齢と体重",ylab="年齢",xlab="体重")
 
 #身長の分布に関するヒストグラムを描画
-hist(children$length)
+hist(children$length,main="身長の分布",xlab="身長")
 #身長の分布に関するヒストグラムを描画
-hist(children$weight)
+hist(children$weight,main="体重の分布",xlab="身長")
 
-#年齢のデータ個数を表示
-sum(table(children$age))
-
-#身長のデータ個数を表示
-sum(table(children$age))
-
-#体重のデータ個数を表示
-sum(table(children$age))
-
-
+#回帰モデルの作成
 result = lm(age~length, data=children)
 summary(result)
 
-
-
-#F検定
-var.test(children$age, children$length)
-
+#モデルを元に予測
 lmpredict = predict(result)
 lmresiduals  = residuals(result)
 
-par(mfrow=c(1,2));
-hist(lmpredict, main="予測値分布",col = "#ff00ff40", border = "#ff00ff")
-hist(lmresiduals , main="残差分布",col = "#0000ff40", border = "#0000ff")
+#予測値と残差の分布をプロット
+par(mfrow=c(1,3));
+plot(lmpredict,children$age,main="年齢の推定値と実測値",ylab="推定値",xlab="実測値")
+hist(lmpredict, main="予測値分布",xlab="予測値",col = "#ff00ff40", border = "#ff00ff")
+hist(lmresiduals , main="残差分布",xlab="残差",col = "#0000ff40", border = "#0000ff")
 
-
+#残差をプロット
 par (mfrow=c(2,2)) 
-plot(lmresult)
+plot(result)
+
+#クック距離を求める
+cooks.distance(result)
+#年齢・身長の実測値と結合
+ck.d=data.frame(children$age,children$length,cooks.distance(result))
+#クック距離0.02以上を抽出
+subset(ck.d,ck.d$cooks.distance.result>0.02)
+
 
 lm.predict = predict(result, interval="prediction")
 lm.confidence=　predict(result,interval="confidence")
