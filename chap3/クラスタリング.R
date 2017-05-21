14a12c713d3af6781ad9f1f4f5f1ce9eaf7fbc66
futurebridge <kengo.nagahashi@futurebridge.jp>
2017-05-22 06:40:28 +0900

    chane
    change

--- chap3/クラスタリング.R
+++ chap3/クラスタリング.R
@@ -1,9 +1,25 @@
+#ユークリッド距離を求める
+国語 = c(80,70,90)
+数学 = c(60,40,30)
 
+点数 = data.frame(国語,数学,row.names=c("田中さん","佐藤さん","鈴木さん"))
+点数
+dist(点数)
+
+#ビールデータの読み込み
+source("https://raw.githubusercontent.com/futurebridge/RBooks/master/chap3/beer.R")
+beer
 
 beer.d = dist(beer)
 round(beer.d,2)
 
+#クラスタリングを実施
 sngl=hclust(beer.d,"single")
+#併合過程を表示
+sngl$merge
+
+
+
 comp=hclust(beer.d) 
 aver=hclust(beer.d,"average")
 ward=hclust(beer.d,"ward.D2")
