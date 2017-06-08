bca6ba0ae1c49dc7f4177529f272b5b98937d8ea
futurebridge <kengo.nagahashi@futurebridge.jp>
2017-06-09 08:47:53 +0900

    change
    change

--- chap2/ポアソン回帰.R
+++ chap2/ポアソン回帰.R
@@ -9,13 +9,15 @@ library(faraway)
 data(gala)
 summary(gala)
 
+#ポアソン回帰のモデル作成
 gala.pm = glm(Species~ ., data = gala, family = poisson)
 summary(gala.pm)
 
+#AICによるパラメータの削減
 gala.pm2 = step(gala.pm)
 summary(gala.pm2)
 
 #モデルをもとに予測する
-predict(gala.pm2,type="respon")
+gala.pre = predict(gala.pm2,type="respon")
 #実測値と比較
 data.frame(gala$Species,round(gala.pre,3),gala$Species-round(gala.pre,3))
